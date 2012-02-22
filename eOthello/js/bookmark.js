eb5f70a82ef40a3517e9870f10cbba2581a991eb
benitez1988 <benitez1988@7da34bd4-3206-707e-89cc-140fc7fae877>
2012-02-22 17:31:12 +0000

    format!

--- eOthello/js/bookmark.js
+++ eOthello/js/bookmark.js
@@ -1,20 +1,1 @@
-var txt = "Bookmark Us!";
-var url = "http://www.eothello.com";
-var who = "eOthello: the Othello battleground";
-
-var ver = navigator.appName;
-var num = parseInt(navigator.appVersion);
-
-if ((ver == "Microsoft Internet Explorer") && (num >= 4)) 
-{			
-	document.write('<a href="javascript:window.external.AddFavorite(url,who);">' + txt + '</a>');								
-}		
-else if (window.sidebar)
-{
-	document.write('<a href="javascript:window.sidebar.addPanel(who, url, \'\');">'+ txt + '</a>');												
-}
-else
-{
-	txt += "  (Ctrl+D)";
-	document.write(txt);
-}
+var txt = "Bookmark Us!";var url = "http://www.eothello.com";var who = "eOthello: the Othello battleground";var ver = navigator.appName;var num = parseInt(navigator.appVersion);if ((ver == "Microsoft Internet Explorer") && (num >= 4)) {				document.write('<a href="javascript:window.external.AddFavorite(url,who);">' + txt + '</a>');								}		else if (window.sidebar){	document.write('<a href="javascript:window.sidebar.addPanel(who, url, \'\');">'+ txt + '</a>');}else{	txt += "  (Ctrl+D)";	document.write(txt);}
